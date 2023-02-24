e722fc765c2dadfdcebf9e9be68ec7439c650fd9
younhuiji <dbsgmlwl_@naver.com>
2023-02-24 20:28:57 +0900

    kakao 주소 검색 api & 테스트 케이스 spock

---
+++ src/main/java/com/example/product/api/service/KakaoUriBuilderService.java
@@ -0,0 +1,26 @@
+package com.example.product.api.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@Slf4j
+@Service
+public class KakaoUriBuilderService {
+
+    private static final String KAKAO_LOCAL_SEARCH_ADDRESS_URL = "https://dapi.kakao.com/v2/local/search/address.json";
+
+    public URI builderByAddressSearch(String address) {
+
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(KAKAO_LOCAL_SEARCH_ADDRESS_URL);
+        uriBuilder.queryParam("query", address);
+
+        URI uri = uriBuilder.build().encode().toUri();
+        log.info("[URI] address: {}, uri: {}", address, uri);
+
+        return uri;
+
+    }
+}
